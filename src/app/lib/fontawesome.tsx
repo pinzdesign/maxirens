31aafde118f3fd62285550aec11cdfc4339786ec
Ivan Popov <tyrael2101@hotmail.com>
2024-11-18 12:22:04 +0100

    boilerplate

---
+++ src/app/lib/fontawesome.tsx
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCalendarDays, faCheckToSlot, faEnvelope, faHashtag, faHouse, faMusic, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faHouse, faMusic, faCalendarDays, faCheckToSlot, faEnvelope, faPhone, faHashtag);
