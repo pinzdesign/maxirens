8237e3bfa8f6cc43671ff45d90e06b77c1360b20
Ivan Popov <tyrael2101@hotmail.com>
2024-11-21 11:47:03 +0100

    serviceboxes

---
+++ src/app/components/ServicesPreview.tsx
@@ -0,0 +1,22 @@
+export default function ServicePreview() {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg servicebox">
+                    <div className="colorLightblue">
+                        <h3>Vask</h3>
+                        <p>Vi tilbyder en række forskellige vaskeservices</p>
+                    </div>
+                    <div className="colorPink">
+                        <h3>Rens</h3>
+                        <p>Vi tilbyder rens af alle typer tekstiler og materialer</p>
+                    </div>
+                    <div className="colorDeepblue">
+                        <h3>Reparation</h3>
+                        <p>Med vores skrædderservice kan vi hjælpe med alle former for reparation af tøj</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ServicePreview from "./components/ServicesPreview";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Maxi Rens</h1>
-        <h2>Subheader</h2>
-        <p>
-          <FontAwesomeIcon icon="house" className="icon"/>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Morbi odio dui, tincidunt sed elit vitae, mollis eleifend ex. 
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-        </p>
-      </main>
-    </div>
-  );
+    <ServicePreview></ServicePreview>
+  )
 }
