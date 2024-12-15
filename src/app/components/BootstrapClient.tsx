9084dace223d2e419f2cae78565712d5cad656dc
Ivan Popov <tyrael2101@hotmail.com>
2024-12-15 15:19:06 +0100

    sd

--- src/app/components/BootstrapClient.tsx
+++ src/app/components/BootstrapClient.tsx
@@ -2,9 +2,16 @@
 
 import { useEffect } from "react";
 
+// Load Bootstrap JS only in the client-side
 function BootstrapClient() {
   useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    if (typeof window !== "undefined" && window.bootstrap) {
+      import("bootstrap/dist/js/bootstrap.bundle.min.js").then(() => {
+        console.log("Bootstrap JavaScript loaded");
+      }).catch((error) => {
+        console.error("Error loading Bootstrap JS:", error);
+      });
+    }
   }, []);
 
   return null;
