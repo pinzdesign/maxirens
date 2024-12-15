873e385a322347baaba2a9d0454b2af8b67331b9
Ivan Popov <tyrael2101@hotmail.com>
2024-12-15 14:45:03 +0100

    tst2

--- src/app/components/BootstrapClient.tsx
+++ src/app/components/BootstrapClient.tsx
@@ -5,7 +5,10 @@ import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function BootstrapClient() {
   useEffect(() => {
-    console.log("Bootstrap JavaScript loaded");
+    // Ensure this code runs only on the client-side
+    if (typeof window !== "undefined") {
+      console.log("Bootstrap JavaScript loaded");
+    }
   }, []);
 
   return null;
